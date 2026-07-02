03e23cc088a31d709aef7a7640a4182a11d3f9c0
Britni Hosfelt <bnbalint@gmail.com>
2026-07-02 16:18:24 -0400

    tests tests tests

---
+++ src/test/java/org/bnbalint/snackdaddy/models/TeamTest.java
@@ -0,0 +1,83 @@
+package org.bnbalint.snackdaddy.models;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.json.JsonContent;
+
+import java.time.Instant;
+import java.time.ZoneOffset;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@JsonTest
+public class TeamTest {
+
+    @Autowired
+    private JacksonTester<Team> jsonTester;
+
+    static Instant DATE = Instant.parse("2026-07-01T00:00:01Z");
+
+    @Test
+    void test_serialization() throws Exception {
+
+        //--------------------------------------------------
+        // SET VALUES
+        Team team = new Team(
+                "Mules",
+                Rink.BAIREL,
+                Level.D5,
+                "#b88907",
+                "#000000",
+                "#c42323",
+                "logo.com"
+        );
+        team.setId(1);
+        team.setCreated(DATE);
+        team.setUpdated(DATE);
+        System.out.println("Team = " + team);
+
+        //--------------------------------------------------
+        // EXECUTE
+        JsonContent<Team> json = jsonTester.write(team);
+        System.out.println("json = " + json);
+
+        //--------------------------------------------------
+        // VERIFY RESULTS
+        assertThat(json).isEqualToJson("team.json");
+        assertThat(json).hasJsonPathNumberValue("$.id");
+        assertThat(json).extractingJsonPathStringValue("$.name").isEqualTo("Mules");
+        assertThat(json).extractingJsonPathStringValue("$.rink").isEqualTo("BAIREL");
+        assertThat(json).extractingJsonPathStringValue("$.level").isEqualTo("D5");
+        assertThat(json).extractingJsonPathStringValue("$.primary_color").isEqualTo("#b88907");
+        assertThat(json).extractingJsonPathStringValue("$.secondary_color").isEqualTo("#000000");
+        assertThat(json).extractingJsonPathStringValue("$.ternary_color").isEqualTo("#c42323");
+        assertThat(json).extractingJsonPathStringValue("$.logo_url").isEqualTo("logo.com");
+        assertThat(json).extractingJsonPathStringValue("$.created").isEqualTo(DATE.atOffset(ZoneOffset.UTC).toString());
+        assertThat(json).extractingJsonPathStringValue("$.updated").isEqualTo(DATE.atOffset(ZoneOffset.UTC).toString());
+    }
+
+
+    @Test
+    void test_deserialization() throws Exception {
+
+        //--------------------------------------------------
+        // SET VALUES
+        Team team = jsonTester.read("team.json").getObject();
+        System.out.println("team = " + team);
+
+        //--------------------------------------------------
+        // VERIFY RESULTS
+        assertThat(team.getId()).isEqualTo(1);
+        assertThat(team.getName()).isEqualTo("Mules");
+        assertThat(team.getRink()).isEqualTo(Rink.BAIREL);
+        assertThat(team.getLevel()).isEqualTo(Level.D5);
+        assertThat(team.getPrimaryColor()).isEqualTo("#b88907");
+        assertThat(team.getSecondaryColor()).isEqualTo("#000000");
+        assertThat(team.getTernaryColor()).isEqualTo("#c42323");
+        assertThat(team.getLogoUrl()).isEqualTo("logo.com");
+        assertThat(team.getCreated()).isEqualTo(DATE.atOffset(ZoneOffset.UTC).toString());
+        assertThat(team.getUpdated()).isEqualTo(DATE.atOffset(ZoneOffset.UTC).toString());
+    }
+}
---
+++ src/test/java/org/bnbalint/snackdaddy/models/UserTest.java
@@ -0,0 +1,110 @@
+package org.bnbalint.snackdaddy.models;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.json.JsonContent;
+
+import java.time.Instant;
+import java.time.ZoneOffset;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@JsonTest
+public class UserTest {
+
+    @Autowired
+    private JacksonTester<User> jsonTester;
+
+    static Instant DATE = Instant.parse("2026-07-01T00:00:01Z");
+
+    @Test
+    void test_serialization() throws Exception {
+
+        //--------------------------------------------------
+        // SET VALUES
+        Team team = new Team(
+                "Mules",
+                Rink.BAIREL,
+                Level.D5,
+                "#b88907",
+                "#000000",
+                "#c42323",
+                ""
+        );
+        team.setId(1);
+        team.setCreated(DATE);
+        team.setUpdated(DATE);
+        Team[] teams = { team };
+
+        Ingredient ingredient = new Ingredient("Pecan");
+        ingredient.setId(1);
+        ingredient.setCreated(DATE);
+        ingredient.setUpdated(DATE);
+        Ingredient[] allergies = { ingredient };
+
+        User user = new User("Roger", "Hogwarts", "r.h@gmail.com", teams, allergies);
+        user.setId(1);
+        user.setCreated(DATE);
+        user.setUpdated(DATE);
+        System.out.println("User = " + user);
+
+        //--------------------------------------------------
+        // EXECUTE
+        JsonContent<User> json = jsonTester.write(user);
+        System.out.println("json = " + json);
+
+        //--------------------------------------------------
+        // VERIFY RESULTS
+        assertThat(json).isEqualToJson("user.json");
+        assertThat(json).hasJsonPathNumberValue("$.id");
+        assertThat(json).extractingJsonPathStringValue("$.first_name").isEqualTo("Roger");
+        assertThat(json).extractingJsonPathStringValue("$.last_name").isEqualTo("Hogwarts");
+        assertThat(json).extractingJsonPathStringValue("$.email").isEqualTo("r.h@gmail.com");
+        assertThat(json).extractingJsonPathArrayValue("$.teams").hasSize(1);
+        assertThat(json).extractingJsonPathArrayValue("$.allergies").hasSize(1);
+        assertThat(json).extractingJsonPathStringValue("$.created").isEqualTo(DATE.atOffset(ZoneOffset.UTC).toString());
+        assertThat(json).extractingJsonPathStringValue("$.updated").isEqualTo(DATE.atOffset(ZoneOffset.UTC).toString());
+    }
+
+
+    @Test
+    void test_deserialization() throws Exception {
+
+        //--------------------------------------------------
+        // SET VALUES
+        User user = jsonTester.read("user.json").getObject();
+        System.out.println("user = " + user);
+
+        //--------------------------------------------------
+        // VERIFY RESULTS
+        assertThat(user.getId()).isEqualTo(1);
+        assertThat(user.getFirstName()).isEqualTo("Roger");
+        assertThat(user.getLastName()).isEqualTo("Hogwarts");
+        assertThat(user.getEmail()).isEqualTo("r.h@gmail.com");
+
+        assertThat(user.getTeams()).hasSize(1);
+        var team = user.getTeams()[0];
+        assertThat(team.getId()).isEqualTo(1);
+        assertThat(team.getName()).isEqualTo("Mules");
+        assertThat(team.getRink()).isEqualTo(Rink.BAIREL);
+        assertThat(team.getLevel()).isEqualTo(Level.D5);
+        assertThat(team.getPrimaryColor()).isEqualTo("#b88907");
+        assertThat(team.getSecondaryColor()).isEqualTo("#000000");
+        assertThat(team.getTernaryColor()).isEqualTo("#c42323");
+        assertThat(team.getLogoUrl()).isEqualTo("");
+        assertThat(team.getCreated()).isEqualTo(DATE.atOffset(ZoneOffset.UTC).toString());
+        assertThat(team.getUpdated()).isEqualTo(DATE.atOffset(ZoneOffset.UTC).toString());
+
+        assertThat(user.getAllergies()).hasSize(1);
+        var allergy = user.getAllergies()[0];
+        assertThat(allergy.getId()).isEqualTo(1);
+        assertThat(allergy.getName()).isEqualTo("Pecan");
+        assertThat(allergy.getCreated()).isEqualTo(DATE.atOffset(ZoneOffset.UTC).toString());
+        assertThat(allergy.getUpdated()).isEqualTo(DATE.atOffset(ZoneOffset.UTC).toString());
+
+        assertThat(user.getCreated()).isEqualTo(DATE.atOffset(ZoneOffset.UTC).toString());
+        assertThat(user.getUpdated()).isEqualTo(DATE.atOffset(ZoneOffset.UTC).toString());
+    }
+}
